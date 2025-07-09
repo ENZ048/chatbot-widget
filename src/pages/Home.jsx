82c83adf0b14906e429ed3ba4cb3882f98fc67b6
Pratik <pratik.yesare68@gmail.com>
2025-07-09 17:56:24 +0530

    Added homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Welcome to the Chatbot Widget</h1>
+      <p>This is the homepage for testing purposes.</p>
+    </div>
+  );
+};
+
+export default Home;
