52d49ac9e89ddf3ba0ff5502c187533d1f533a1a
Pratik <pratik.yesare68@gmail.com>
2025-07-09 18:31:18 +0530

    Fixed main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import EmbedPage from "./pages/EmbedPage"; // 👈 create this
+import { Routes, Route } from "react-router-dom";
+import EmbedPage from "./pages/EmbedPage";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/embed/:chatbotId" element={<EmbedPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/embed/:chatbotId" element={<EmbedPage />} />
+    </Routes>
   );
 }
 
